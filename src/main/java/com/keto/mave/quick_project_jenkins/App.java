9d00fc5bb18ba611858f1b2f84744d504a263548
root <root@oc1042172637.ibm.com>
2020-06-03 07:04:22 -0500

    First commit

---
+++ src/main/java/com/keto/mave/quick_project_jenkins/App.java
@@ -0,0 +1,13 @@
+package com.keto.mave.quick_project_jenkins;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/com/keto/mave/quick_project_jenkins/AppTest.java
@@ -0,0 +1,20 @@
+package com.keto.mave.quick_project_jenkins;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
